9e24e78895f20a6a1b635cf77fae014bb477f004
Spajkowiecki <pcio10@wp.pl>
2022-12-28 20:49:42 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,16 +1,36 @@
 import _ from 'lodash';
 
 const form = document.querySelector('form');
+const emailInput = form.querySelector('label > input');
+let textInput = form.querySelector('label > textarea');
 //input listener
 
 //listening for changes in input fields
 form.addEventListener(
   'input',
   _.throttle(e => {
-    const {
-      elements: { email, message },
-    } = e.currentTarget;
-    let data = { email: email.value, message: message.value };
+    let data = {
+      email: emailInput.value,
+      message: textInput.value,
+    };
     localStorage.setItem('feedback-form-state', JSON.stringify(data));
   }, 500)
 );
+
+let getData = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+if (localStorage.getItem('feedback-form-state') === null) {
+  emailInput.value = '';
+  textInput.value = '';
+} else {
+  emailInput.value = getData.email;
+  if (getData.textInput === '') textInput.value = '';
+}
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  console.log(localStorage.getItem('feedback-form-state'));
+  emailInput.value = '';
+  textInput.value = '';
+  localStorage.removeItem('feedback-form-state');
+});
